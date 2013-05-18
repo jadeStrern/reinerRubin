c8693f6ba4e2aef39282033d3c4b7a2516cd9f50
kotya <kotya@kotya-laptop.(none)>
2013-05-18 22:35:12 +0400

    translit

---
+++ reinerRubin_ru.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PLayerManager</name>
+    <message>
+        <location filename="playermanager.cpp" line="60"/>
+        <source>statusReplied</source>
+        <translation type="unfinished">ответ</translation>
+    </message>
+</context>
+<context>
+    <name>PlayersGroup</name>
+    <message>
+        <location filename="playersgroup.cpp" line="21"/>
+        <location filename="playersgroup.cpp" line="26"/>
+        <source>statusNotReplied</source>
+        <translation type="unfinished">не ответ</translation>
+    </message>
+    <message>
+        <location filename="playersgroup.cpp" line="23"/>
+        <source>actionDropAnsw</source>
+        <translation type="unfinished">прек. ответ</translation>
+    </message>
+    <message>
+        <location filename="playersgroup.cpp" line="25"/>
+        <source>actionReply</source>
+        <translation type="unfinished">отв.</translation>
+    </message>
+</context>
+<context>
+    <name>StationWidgetManager</name>
+    <message>
+        <location filename="stationwidgetmanager.cpp" line="10"/>
+        <source>actionReply</source>
+        <translation type="unfinished">отв.</translation>
+    </message>
+    <message>
+        <location filename="stationwidgetmanager.cpp" line="14"/>
+        <source>statusNotReplied</source>
+        <translation type="unfinished">не ответ</translation>
+    </message>
+</context>
+</TS>
